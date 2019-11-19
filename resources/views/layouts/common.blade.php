cf1ac7c0f7492e3e05221b7abebaa42476b9d8ec
kawada <kawada@MAC268.local>
2019-11-19 15:04:28 +0900

    headerとフッターのデザインの変更

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -6,17 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<nav>
+    <ul>
+        <li class=”current”><a href=”#”>ホーム</a></li>
+        <li><a href=”#”>アレンジデータ</a></li>
+        <li><a href=”#”>利用規約</a></li>
+        <li><a href=”#”>プライバシーポリシー</a></li>
+        <li><a href=”#”>お問い合わせ</a></li>
+    </ul>
+</nav>
+       @yield('content')
+<footer>
+    <p>© 2019 darts-arrange All rights Reserved.</p>
+</footer>
 
-</head>
-<body>
-    <header>
-        <h1>Laravel Blog</h1>
-    </header>
-
-@yield('content')
-
-    <footer>
-        <small>copyrights &copy; 2018 kotsuban-teikin All rights Reserved.</small>
-    </footer>
 </body>
 </html>
