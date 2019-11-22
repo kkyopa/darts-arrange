c152f60f662bddb08f91a3482024a4e96772622b
kawada <kawada@MAC268.local>
2019-11-22 16:23:31 +0900

    コンタクト機能を作成

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.common')
 @section('content')
 
+
+<div class="description">
+    <p>
+        サービスに対するご意見・ご要望などがございましたら、お問い合わせはTwitterからお気軽にお問い合わせください。
+    </p>
+    <p>
+    お返事にお時間をいただく場合がございます。予めご了承いただきますようお願いいたします。
+    </p>
+</div>
+
+<img alt="文字表示" class="twitter_icon"  src="{{ asset('/img/twitter-icon.jpg') }}">
 @endsection
