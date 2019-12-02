4500975254ad237c13e3d7ce08284259104788d9
kawada <kawada@MAC268.local>
2019-12-02 11:12:03 +0900

    データベーステスト

---
+++ database/factories/OpenoutFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+// use Faker\Generator as Faker;
+
+// $factory->define(App\Openout::class, function (Faker $faker) {
+//     return [
+//         'user_id' => \App\User::find(1)->id,
+//         'arrangefirst' => 0,
+//         'arrangesecond' => 0,
+//         'arrangethird' => 0,
+//         'arrangememo' => 'アレンジメモ',
+//     ];
+// });
