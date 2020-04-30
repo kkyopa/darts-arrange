f338078f9e1f542a7fd5936825457c0666aef054
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2020-04-30 15:48:48 +0900

    edit画面バリデーションエラー表示

--- resources/views/openout/edit.blade.php
+++ resources/views/openout/edit.blade.php
@@ -7,24 +7,37 @@
     <title>掲示板</title>
 </head>
 <body>
-
 <h1>更新画面</h1>
-スコア
-{{ $openout->arrangenumber }}
+
+<h3>オープンアウト入力説明</h3>
 <br>
-1投目
-{{ $openout->arrangefirst }}
+<p>①スコアは任意の数字を入力</p>
+<p>②スコアを入力する際、最初の文字は「TかDかSの大文字を選択し入力した上で大文字の数字（１~２０）を入力してください（BULLの場合はBULLと入力）</p>
+<p>③メモが必要な場合はアレンジメモに記述</p>
+<p>④アレンジメモを確認したい、アレンジの変更、削除は詳細ボタンで変更できます。</p>
 <br>
-2投目
-{{ $openout->arrangesecond }}
-<br>
-3投目
-{{ $openout->arrangethird }}
-<br>
-メモ
-{{ $openout->arrangememo }}
 
 
+<p>変更前のアレンジ:</p>
+<input name="ex_arrangenumber" value="{{ $openout->arrangenumber }}">
+スコア<br>
+<input name="ex_arrangefirst" value="{{ $openout->arrangefirst }}">
+１投目<br>
+<input name="ex_arrangesecond" value="{{ $openout->arrangesecond }}">
+２投目<br>
+<input name="ex_arrangethird" value="{{ $openout->arrangethird }}">
+３投目<br>
+<textarea name="ex_arrangememo" rows="4" cols="40">{{ $openout->arrangememo }}</textarea>
+アレンジメモ<br><br><br>
+
+
+@if(count($errors) > 0)
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 
 <form method="POST" action="{{ url('openout/update', ['id' => $openout->id ]) }}">
 {{ csrf_field() }}
