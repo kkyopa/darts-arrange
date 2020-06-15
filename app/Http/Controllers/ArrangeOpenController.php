d2540aae7ebdaa7e5383a71a717446800007525f
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2020-06-15 14:18:43 +0900

    トップのボタンをブーストラップのクラス指定

--- app/Http/Controllers/ArrangeOpenController.php
+++ app/Http/Controllers/ArrangeOpenController.php
@@ -13,6 +13,7 @@ class ArrangeOpenController extends Controller
     public function index(Request $request) {
         $authUser = Auth::user();
         $query = Openout::select('id','user_id', 'arrangenumber', 'arrangefirst', 'arrangesecond', 'arrangethird', 'arrangememo',DB::raw('count(*) as count'));
+        // withメゾットでuserをリレーションさせる
         $query->with(['user']);
         $keyword = $request->input('keyword');
         $rating = $request->input('rating');
@@ -20,18 +21,13 @@ class ArrangeOpenController extends Controller
             $query->where('arrangenumber', 'LIKE', "{$keyword}");
         }
 
+        // whereHasでuserがリレーションされていれば第二引数が呼び出される
         if (!empty($rating)) {
             $query->whereHas('user', function ($query) use ($rating) {
                 $query->where('rating', $rating);
             });
         }
 
-        // ->orWhere('rating', 'LIKE', "%$rating%")
-        // $post = User::with('rating')->find(1);
-        // return $post;
-        // return $this->belongsTo('App\User')->where('rating');
-
-        // $query->select('id','user_id', 'arrangenumber', 'arrangefirst', 'arrangesecond', 'arrangethird', 'arrangememo',DB::raw('count(*) as count'));
         $query->groupBy('arrangefirst', 'arrangesecond', 'arrangethird');
         // $query->orderBy('arrangenumber', 'asc');
         $query->orderByRaw('COUNT(*) DESC');
