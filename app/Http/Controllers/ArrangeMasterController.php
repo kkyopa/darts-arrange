cecb6f4c9f4c8e8575eb7b800e8c97101063ffc5
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2020-06-16 11:46:28 +0900

    マスターアウト検索機能作成のためオープンアウトからコードをコピー

--- app/Http/Controllers/ArrangeMasterController.php
+++ app/Http/Controllers/ArrangeMasterController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Masterout;
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ArrangeMasterController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $authUser = Auth::user();
+        $query = Masterout::select('id','user_id', 'arrangenumber', 'arrangefirst', 'arrangesecond', 'arrangethird', 'arrangememo',DB::raw('count(*) as count'));
+        // withメゾットでuserをリレーションさせる
+        $query->with(['user']);
+        $keyword = $request->input('keyword');
+        $rating = $request->input('rating');
+        if (!empty($keyword)) {
+            $query->where('arrangenumber', 'LIKE', "{$keyword}");
+        }
+
+        // whereHasでuserがリレーションされていれば第二引数が呼び出される
+        if (!empty($rating)) {
+            $query->whereHas('user', function ($query) use ($rating) {
+                $query->where('rating', $rating);
+            });
+        }
+
+        $query->groupBy('arrangefirst', 'arrangesecond', 'arrangethird');
+        $query->orderByRaw('COUNT(*) DESC');
+        $masterout = $query->paginate(10);
+        return view('/arrange-data/masterout_data', compact('masterout','authUser','keyword','rating'));
+    }
 }
--- resources/views/arrange-data/masterout_data.blade.php
+++ resources/views/arrange-data/masterout_data.blade.php
@@ -1,5 +1,66 @@
 @extends('layouts.app')
 @section('content')
 
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>掲示板</title>
+</head>
+<body>
+
+<h1>オープンアウト編</h1>
+<br>
+<h3>オープンアウト説明</h3>
+<br>
+<p>①検索したいスコアとフライトを選択</p>
+<p>②検索をクリックすると条件にあったものが表示されます!!</p>
+<br>
+
+
+<h1>検索</h1>
+<br>
+<form method="GET" action="{{ url('../arrange-data/masterout_data') }}" class="form-inline my-2 my-lg-0">
+    <h5>スコア</h5>
+    <p><input type="text" class="form-control mr-sm-2" name="keyword" value="{{$keyword}}"></p><br>
+    <p>フライト<br>
+    <input type="radio" name="rating" value="A"> Aフライト
+    <input type="radio" name="rating" value="B"> Bフライト
+    <input type="radio" name="rating" value="C"> Cフライト
+    </p><br>
+    <p><input class="btn btn-outline-success my-2 my-sm-0" type="submit" value="検索"></p>
+</form>
+
+
+<table class ="table">
+    <thead>
+        <tr>
+        <th scope="col">スコア (件数)</th>
+        <th scope="col">1投目</th>
+        <th scope="col">2投目</th>
+        <th scope="col">3投目</th>
+        <th scope="col">詳細</th>
+        </tr>
+    </thead>
+    <tbody>
+    @if($masterout->count())
+        @foreach($masterout as $d)
+                <tr>
+                <th>{{ $d->arrangenumber }}({{ $d->count}})</th>
+                <td>{{ $d->arrangefirst }}</td>
+                <td>{{ $d->arrangesecond }}</td>
+                <td>{{ $d->arrangethird }}</td>
+            </tr>
+        @endforeach
+        @else
+            <p>見つかりませんでした。</p>
+        @endif
+</tbody>
+</table>
+<br><hr>
+
+{{ $masterout->render() }}
+
+</body>
+</html>
 
 @endsection
