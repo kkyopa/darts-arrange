899e0796744c365037629d93e6618f7dc69b6fac
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2020-06-26 16:45:09 +0900

    パーフェクト編のバリデーション実装

--- app/Http/Requests/PerfectRequest.php
+++ app/Http/Requests/PerfectRequest.php
@@ -24,8 +24,26 @@ class PerfectRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'arrangefirst_type'=>['required'],
-        ];
+        // dd($this->arrangefirst_type);
+        $rule = [];
+        $rule['arrangefirst_type'] = ['required'];
+        if (isset($this->arrangefirst_type) && $this->arrangefirst_type  !== 'DBULL' && $this->arrangefirst_type  !== 'SBULL') {
+            $rule['arrangefirst_score'] = ['required'];
+        }
+        // secondのバリデーション
+        if (isset($this->arrangesecond_type) && $this->arrangesecond_type  !== 'DBULL' && $this->arrangesecond_type  !== 'SBULL') {
+            $rule['arrangesecond_score'] = ['required'];
+        }
+        if (isset($this->arrangesecond_score) && $this->arrangesecond_type  !== 'DBULL' && $this->arrangesecond_type  !== 'SBULL') {
+            $rule['arrangesecond_type'] = ['required'];
+        }
+        // thirdのバリデーション
+        if (isset($this->arrangethird_type) && $this->arrangethird_type  !== 'DBULL' && $this->arrangethird_type  !== 'SBULL') {
+            $rule['arrangethird_score'] = ['required'];
+        }
+        if (isset($this->arrangethird_score) && $this->arrangethird_type  !== 'DBULL' && $this->arrangethird_type  !== 'SBULL') {
+            $rule['arrangethird_type'] = ['required'];
+        }
+        return $rule;
     }
 }
